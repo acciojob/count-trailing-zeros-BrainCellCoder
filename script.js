2a2d8a7c29211dfb236dd0aa346c477e8758cf2e
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-19 09:42:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
-	const zeros = [];
-	const nArray = n.split("");
-
-	if(nArray.length == 1) return 0;
-	
-	for(let i=nArray.length-1;i>=0;i--){
-		if(nArray[i] == 0){
-			zeros.push(nArray[i])
-		}
-		else return zeros.length;
-	}
-}
-
-const input = prompt("Enter a number");
-alert(trailingZeros(input));
+      const nArray = n.split("");
+      if (nArray.length === 1) return 0;
+      let zeroCount = 0;
+      for (let i = nArray.length - 1; i >= 0; i--) {
+        if (nArray[i] === '0') {
+          zeroCount++;
+        } else {
+          break;
+        }
+      }
+      return zeroCount;
+    }
+    const input = prompt("Enter a number");
+    alert(trailingZeros(input));
